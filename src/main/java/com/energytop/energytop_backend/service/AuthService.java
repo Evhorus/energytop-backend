bd08dcc769ae9506727976eb6eac92812882efe1
Evhorus <sesbas778@gmail.com>
2024-10-16 15:55:05 -0500

    Se crea la estructura del proyecto

--- src/main/java/com/energytop/energytop_backend/service/AuthService.java
+++ src/main/java/com/energytop/energytop_backend/service/AuthService.java
@@ -6,8 +6,16 @@ import org.springframework.stereotype.Service;
 import com.energytop.energytop_backend.repository.UserRepository;
 
 @Service
-public class UserService {
+public class AuthService {
   @Autowired
   UserRepository userRepository;
+
+  public String createUser(){
+    return "createUser";
+  }
+
+  public String login(){
+    return "login";
+  }
 
 }
